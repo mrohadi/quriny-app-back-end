9862f624d7016c66c698955838021923537a4f63
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-06-21 20:41:49 +0700

    Refactorizing & Organizing

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+module.exports = {
+  apiKey: "AIzaSyDbhN0izkOP9gN4XxKZ1zsc92Lpo4fhAIU",
+  authDomain: "social-media-app-33923.firebaseapp.com",
+  databaseURL: "https://social-media-app-33923.firebaseio.com",
+  projectId: "social-media-app-33923",
+  storageBucket: "social-media-app-33923.appspot.com",
+  messagingSenderId: "795255708610",
+  appId: "1:795255708610:web:a6bea33c3530e0ae2ed45b",
+  measurementId: "G-V23N68SHYV",
+};
