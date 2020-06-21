0be52395a76b71d1fd4b3c8bed4153d316438c83
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-06-21 12:52:56 +0700

    Sixing setting the database

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "This is the scream body",
+      createdAt: "2020-06-21T05:42:06.207Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,53 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const express = require("express");
+const app = express();
+
+// Route to see all the scream in the data base
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+// Route Post or create a new scream
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created succesfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+// https://baseurl.com/api/
+exports.api = functions.region("asia-east2").https.onRequest(app);
