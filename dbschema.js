9ce6608e730c023db08034773485f97766148273
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-06-23 14:59:03 +0700

    getting and commenting

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: "dh23ggj5h32g543j5gf43",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2019-03-15T10:59:52.798Z",
+      imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+      bio: "Hello, my name is user, nice to meet you",
+      website: "https://user.com",
+      location: "Lonodn, UK",
+    },
+  ],
   screams: [
     {
       userHandle: "user",
@@ -6,6 +18,38 @@ let db = {
       createdAt: "2020-06-21T05:42:06.207Z",
       likeCount: 5,
       commentCount: 2,
+    },
+  ],
+  comments: [
+    {
+      userHandle: "user",
+      screamId: "kdjsfgdksuufhgkdsufky",
+      body: "nice one mate!",
+      createdAt: "2019-03-15T10:59:52.798Z",
+    },
+  ],
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:52.798Z",
+    imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "Lonodn, UK",
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "hh7O5oWfWucVzGbHH2pa",
+    },
+    {
+      userHandle: "user",
+      screamId: "3IOnFoQexRcofs5OhBXO",
     },
   ],
 };
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const FBauth = require("./util/fbAuth");
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+} = require("./handlers/screams");
 const {
   signup,
   login,
@@ -13,6 +18,12 @@ const {
 // Screams Route
 app.get("/screams", getAllScreams);
 app.post("/scream", FBauth, postOneScream);
+app.get("/scream/:screamId", getScream);
+// TODO delete scream
+// TODO like a scream
+// TODO unlike a scream
+app.post("/scream/:screamId/comment", FBauth, commentOnScream);
+
 // Users Route
 app.post("/signup", signup);
 app.post("/login", login);
