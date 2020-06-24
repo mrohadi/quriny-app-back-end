bfdc4c84c10c15087e07fafba0421826cd39f577
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-06-25 01:46:20 +0700

    signup and authenticate state

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,30 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import jwtDecode from "jwt-decode";
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 // Components
 import Navbar from "./components/Navbar";
+// Util
+import themeFile from "./util/theme";
+import AuthRoute from "./util/AuthRoute";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#9ccc65",
-    },
-    secondary: {
-      main: "#ccff90",
-    },
-  },
-  typography: {
-    useNextVariants: true,
-  },
-});
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
@@ -33,8 +37,18 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
---
+++ client/src/util/theme.js
@@ -0,0 +1,41 @@
+export default {
+  palette: {
+    primary: {
+      main: "#9ccc65",
+    },
+    secondary: {
+      main: "#ccff90",
+    },
+  },
+  // the object to be spread
+  spreadThis: {
+    typography: {
+      useNextVariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      maxWidth: "15%",
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textFiled: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      marginTop: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
